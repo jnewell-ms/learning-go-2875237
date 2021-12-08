74af3680f6981d4c04c5456c9a9ad9a9bb272d80
= <=>
2021-12-08 10:47:25 -0600

    changes going with video

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	var aString string = "This is Go!"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	//NOTE: Characters are wrapped in single quotes, strings are wrapped in double quotes
+	//error output is ignored using '_'
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered:", input)
+
+	fmt.Print("Enter a number: ")
+	numInput, _ := reader.ReadString('\n')
+	//Expect there to possibly be empty space before the number, so use strings.TrimSpace to deal with that, also using 64 bits ~ since 64 bit processor
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+
+	//This is checking if there is an error
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number:", aFloat)
+	}
 
 }
